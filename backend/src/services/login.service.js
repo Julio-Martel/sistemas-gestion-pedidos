21393d6b5fef1756e0ee4131bcfb8948501e4161
Julio-Martel <julioramdom123@gmail.com>
2026-06-01 18:24:54 -0300

    se ha arreglado la funcion para el retorno del token

--- backend/src/services/login.service.js
+++ backend/src/services/login.service.js
@@ -1,8 +1,33 @@
 import jwt from 'jsonwebtoken';
-import db from '../config/db.js';
+import bcrypt from 'bcrypt';
+import { verificarEmail } from '../models/login.model.js';
+
 
 const logearUsuario = async(email,pass) => {
-        
+
+    if(!email || !pass){
+         throw new Error('DEBE INGRESAR EL MAIL Y EL PASS');
+    }
+
+    const comprobarEmail = await verificarEmail(email);
+
+    if(comprobarEmail === undefined){
+        throw new Error('EMAIL NO EXISTENTE');
+    }
+
+    const comprobarPassword =  await bcrypt.compare(pass,comprobarEmail.pass);
+
+    const token = jwt.sign(
+        { id: comprobarEmail.id,
+          rol: comprobarEmail.rol,
+          pass: comprobarEmail.pass
+        },
+        'secreto',
+        {expiresIn: '1h'}
+    )
+
+    return token;
+
 }
 
 export {
