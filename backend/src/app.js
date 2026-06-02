a378ed91919b5e879aeb150bfc2d86376b8faa6d
Julio-Martel <julioramdom123@gmail.com>
2026-06-02 18:39:29 -0300

    se ha añadido el handler loginRouter

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import loginRouter from './routes/login.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/login', );
+app.use('/login', loginRouter);
 app.use('/register', );
 app.use('/cliente', );
 app.use('/duenio', );
