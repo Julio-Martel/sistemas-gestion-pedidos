a6e36b725fc81ae7a2b2dab62c207053f81c338f
Julio-Martel <julioramdom123@gmail.com>
2026-06-06 18:33:04 -0300

    se agregaron las funciones y consultas de sql para evitar mail repetido y la creacion del usuario

--- backend/src/models/register.model.js
+++ backend/src/models/register.model.js
@@ -1,9 +1,26 @@
 import db from '../config/db.js';
-import { verificarEmail } from './login.model.js';
+
+const verificarEmailRepetido = async(email) => {
+    const [resultado] = await db.query(
+        `SELECT email FROM Usuarios 
+         WHERE email = ?`,[email]
+    );
+    
+    return resultado;
+}
+
+const crearUsuario = async (data, hash) => {
+    const [resultado] = await db.query(
+        `INSERT INTO Usuarios(nombre,email,pass,rol)
+         VALUES(?,?,?,?)`,
+        [data.nombre, data.email, hash, data.rol]
+    );
+
+    return resultado;
+}
 
 
-/*
-    COMPLETAR ACA PARA LO DE VERIFICAR MAIL
-
-
-*/
+export {
+    verificarEmailRepetido,
+    crearUsuario
+}
