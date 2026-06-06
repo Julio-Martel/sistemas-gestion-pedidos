a3a8cc8d5351df280a0e8d9d2457d181c91c3bd0
Julio-Martel <julioramdom123@gmail.com>
2026-06-05 21:05:19 -0300

    se ha creado la ruta para la creacion del registro de usuarios

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import loginRouter from './routes/login.routes.js';
+import { registerRouter } from './routes/register.router.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/login', loginRouter);
-/*app.use('/register', );
-app.use('/cliente', );
+app.use('/register', registerRouter);
+/*app.use('/cliente', );
 app.use('/duenio', );*/
 
 export default app;
