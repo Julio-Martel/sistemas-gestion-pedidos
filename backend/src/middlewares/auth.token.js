5ed2fb154d647ae618942ada978e2cd9ea50c953
Julio-Martel <julioramdom123@gmail.com>
2026-06-11 20:36:04 -0300

    se ha realizado la estructura de lo que seria la autenticacion del token

--- backend/src/middlewares/auth.token.js
+++ backend/src/middlewares/auth.token.js
@@ -1,6 +1,24 @@
-const autenticarToken = async(req,res,next) => {
-    const headerToken = req.headers.authentication;
-}
+import bcrypt from 'bcrypt';
 
+const autenticarToken = async(req,res,next) => {
+    const headerToken = req.headers.authorization;
+
+    if(!headerToken){
+        return res.send('No hay token')
+    }
+
+    const ht = headerToken.split(' ')[1];
+
+    try{
+        const decoded = await bcrypt.compare(ht,'secreto');
+
+        req.usuario = decoded;
+
+        next();
+
+    } catch(error){
+        res.status(403).json({ mensaje: 'Token inválido' });
+    }
+}
 
 export {autenticarToken};
