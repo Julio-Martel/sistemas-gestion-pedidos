2a2780fb39ed5244fe9343f6d665ef09a004b696
Julio-Martel <julioramdom123@gmail.com>
2026-06-12 20:32:44 -0300

    se creo el archivo js clientes.routes

--- backend/src/routes/register.router.js
+++ backend/src/routes/register.router.js
@@ -3,8 +3,6 @@ import { registerController } from '../controllers/register.controller.js';
 
 const registerRouter = express.Router();
 
-registerRouter.post();
+registerRouter.post('/', registerController);
 
-export {
-    registerRouter
-};
+export default registerRouter;
