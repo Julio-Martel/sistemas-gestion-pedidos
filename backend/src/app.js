84fb13ba209e35928189307b3f036214811ac9e2
Julio-Martel <julioramdom123@gmail.com>
2026-06-15 13:05:22 -0300

    se ha agregado el handler de la ruta del cliente

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import loginRouter from './routes/login.routes.js';
-import { registerRouter } from './routes/register.router.js';
+import registerRouter from './routes/register.router.js';
+import clientesRoutes from './routes/clientes.routes.js';
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(express.json());
 
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
-/*app.use('/cliente', );
-app.use('/duenio', );*/
+app.use('/cliente', clientesRoutes);
+/*app.use('/duenio', );*/
 
 export default app;
