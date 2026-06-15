8a738da10847742e500701928dfe4d35eaff2b3b
Julio-Martel <julioramdom123@gmail.com>
2026-06-14 23:13:22 -0300

    se ha agregado el autenticador de token

--- backend/src/routes/clientes.routes.js
+++ backend/src/routes/clientes.routes.js
@@ -1,5 +1,9 @@
-/*
-    AGREGAR AQUI LA LOGICA DE RUTAS Y CONTROLADORES PARA EL CLIENTE
+import express from 'express';
+import { verProductos } from '../services/cliente.service.js';
+import { autenticarToken } from '../middlewares/auth.token.js';
 
+const clientesRoutes = express.Router();
 
-*/
+clientesRoutes.get(autenticarToken);
+
+export default clientesRoutes;
