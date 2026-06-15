27df345fa0fbd226d227b788e6148cebbc40e373
Julio-Martel <julioramdom123@gmail.com>
2026-06-15 12:52:08 -0300

    se ha completado el controlador de ver productos

---
+++ backend/src/controllers/cliente.controller.js
@@ -0,0 +1,23 @@
+import { obtenerTodosLosProductos } from "../services/cliente.service.js"
+
+const verProductos = async(req,res) => {
+    try{
+        const todosLosProductos = await obtenerTodosLosProductos();
+
+        res.status(200).json({
+            mensaje: 'Todos los productos',
+            productos: todosLosProductos
+        })
+
+    } catch(error){
+        if(error.message === 'NO EXISTEN PRODUCTOS'){
+            return res.status(404).json({
+                mensaje: 'No hay productos en la base de datos'
+            })
+        }
+    }
+}
+
+export {
+    verProductos
+}
