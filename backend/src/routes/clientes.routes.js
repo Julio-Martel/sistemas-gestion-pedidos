a9f9ae64b8b9384e0a7560f4addccf4da2bfc6d2
Julio-Martel <julioramdom123@gmail.com>
2026-06-15 00:23:36 -0300

    se ha cambiado el permiso para el cliente

--- backend/src/routes/clientes.routes.js
+++ backend/src/routes/clientes.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { verProductos } from '../services/cliente.service.js';
 import { autenticarToken } from '../middlewares/auth.token.js';
-import { permisos } from '../middlewares/auth.permisos.js';
+import { permisoCliente } from '../middlewares/auth.permisos.js';
 
 const clientesRoutes = express.Router();
 
-clientesRoutes.get(autenticarToken);
+clientesRoutes.get(autenticarToken, permisoCliente);
 
 export default clientesRoutes;
