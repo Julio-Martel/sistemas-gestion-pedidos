b37d52d609aa46f18c9691218439061f48d36c32
Julio-Martel <julioramdom123@gmail.com>
2026-06-15 13:08:46 -0300

    se ha cambiado la importacion de la funcion del controlador para ver productos

--- backend/src/routes/clientes.routes.js
+++ backend/src/routes/clientes.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verProductos } from '../services/cliente.service.js';
+import { verProductos } from '../controllers/cliente.controller.js';
 import { autenticarToken } from '../middlewares/auth.token.js';
 import { permisoCliente } from '../middlewares/auth.permisos.js';
 
