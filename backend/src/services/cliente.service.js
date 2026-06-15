f28408d7bf59ada1c507bbeed2466ca53a503894
Julio-Martel <julioramdom123@gmail.com>
2026-06-15 02:20:41 -0300

    Se ha completado la funcion del servicio de get productos

--- backend/src/services/cliente.service.js
+++ backend/src/services/cliente.service.js
@@ -1,7 +1,13 @@
 import { getProductos } from "../models/cliente.model.js"
 
 const obtenerTodosLosProductos = async() => {
+    const productos = await getProductos();
 
+    if(productos.length === 0){
+        throw new Error(`NO EXISTEN PRODUCTOS`)
+    }
+
+    return productos; 
 }
 
 
