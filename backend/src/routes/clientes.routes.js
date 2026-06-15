3245fd3beffc036d92a284c4cc22989efaf76af1
Julio-Martel <julioramdom123@gmail.com>
2026-06-14 23:53:42 -0300

    se han agregado los permisos

--- backend/src/routes/clientes.routes.js
+++ backend/src/routes/clientes.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { verProductos } from '../services/cliente.service.js';
 import { autenticarToken } from '../middlewares/auth.token.js';
+import { permisos } from '../middlewares/auth.permisos.js';
 
 const clientesRoutes = express.Router();
 
