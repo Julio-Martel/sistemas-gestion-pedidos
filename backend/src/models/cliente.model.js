d7dff1d0a19498727369fba9f2c971ad7f0e420c
Julio-Martel <julioramdom123@gmail.com>
2026-06-17 19:09:45 -0300

    se ha agregado la funcion de crear pedido

--- backend/src/models/cliente.model.js
+++ backend/src/models/cliente.model.js
@@ -6,7 +6,29 @@ const getProductos = async() => {
     return resultados;
 }
 
+const getCliente = async(id_cliente) => {
+    const [resultados] = await db.query(`SELECT * FROM Usuarios 
+        WHERE id = ?`, [id_cliente]);
+
+    return resultados;
+}
+
+const getTienda = async(id_tienda) => {
+    const [resultados] = await db.query(`SELECT * FROM Tiendas 
+        WHERE id = ?`,[id_tienda]);
+
+    return resultados;
+}
+
+const createPedido = async() => {
+   
+}
+
+
 
 export {
-    getProductos
+    getProductos,
+    createPedido,
+    getCliente,
+    getTienda
 }
