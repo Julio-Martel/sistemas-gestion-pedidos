ab5fd02b357ce00ae9af5de983a6eef065d0355b
Julio-Martel <julioramdom123@gmail.com>
2026-06-21 22:44:29 -0300

    se ha agregado la estructura de la funcion asincrona

--- backend/src/controllers/duenio.controller.js
+++ backend/src/controllers/duenio.controller.js
@@ -1,7 +1,23 @@
+import { generacionDeTienda } from "../services/duenio.service.js"
+
 const crearTienda = async(req,res) => {
+    
     if(!req.body || Object.keys(req.body).length === 0){
         return res.send('No se acepta el body vacio')
     }    
+
+    try {
+      const tienda = await generacionDeTienda(req.body);
+   
+   
+   
+    } catch(error){
+        
+        res.status(500).json({
+            mensaje: 'ERROR INTERNO',
+            err: error
+        })
+    }
 } 
 
 
