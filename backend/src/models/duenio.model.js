be64b3413cadc5148b003dac4ca14fbe3c7f6ecd
Julio-Martel <julioramdom123@gmail.com>
2026-06-21 23:56:01 -0300

    se ha creado la funcion de crear tienda para la consulta sql

--- backend/src/models/duenio.model.js
+++ backend/src/models/duenio.model.js
@@ -1,7 +1,12 @@
 import db from '../config/db.js';
 
-const createTienda = async(data) => {
 
+const createTienda = async(data) => {
+    const [resultado] = await db.query(`INSERT INTO Tiendas(nombre,direccion,id_duenio,telefono)
+        VALUES(?,?,?,?)`,
+        [data.nombre, data.direccion, data.id_duenio, data.telefono]);
+    
+    return resultado;
 }
 
 export {
