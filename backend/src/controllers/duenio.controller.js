cf9ed8d2791a2fe86c06dd05c8fca39b679eca3f
Julio-Martel <julioramdom123@gmail.com>
2026-06-23 19:22:19 -0300

    se cambiaron el numero de los status

--- backend/src/controllers/duenio.controller.js
+++ backend/src/controllers/duenio.controller.js
@@ -3,16 +3,20 @@ import { generacionDeTienda } from "../services/duenio.service.js"
 const crearTienda = async(req,res) => {
     
     if(!req.body || Object.keys(req.body).length === 0){
-        return res.send('No se acepta el body vacio')
+        return res.status(400).json({
+            mensaje: 'No se acepta el body vacio'
+        })
     }    
 
     try {
       const tienda = await generacionDeTienda(req.body);
    
-   
+      res.status(201).json({
+        mensaje: 'Tienda creada con exito!'
+      })
    
     } catch(error){
-        
+
         res.status(500).json({
             mensaje: 'ERROR INTERNO',
             err: error
