569a4815064b049275eee5860c63dcc0a1077f26
Julio-Martel <julioramdom123@gmail.com>
2026-06-23 19:07:45 -0300

    se ha importado la funcion de crear tienda

--- backend/src/routes/duenio.routes.js
+++ backend/src/routes/duenio.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { autenticarToken } from '../middlewares/auth.token.js';
 import { permisoDuenio } from '../middlewares/auth.permisos.js';
+import { crearTienda } from '../controllers/duenio.controller.js';
 
 const duenioRoutes = express.Router();
 
