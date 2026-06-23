99dee651faf41402a1423a52b7ce4812dfea86a2
Julio-Martel <julioramdom123@gmail.com>
2026-06-23 19:30:45 -0300

    se han agregado algunas validaciones de negocio

--- backend/src/services/duenio.service.js
+++ backend/src/services/duenio.service.js
@@ -1,13 +1,24 @@
 import { createTienda } from "../models/duenio.model.js";
 
 const generacionDeTienda = async(data) => {
-  const crearTienda = await createTienda(data);
+    if(!data.nombre){
+        throw new Error(`Nombre requerido`);
+    }
+  
+    if(!data.direccion){
+        throw new Error(`Direccion requerida`);
+    }
+        
+    if(!data.telefono){
+        throw new Error(`Telefono requerido`);
+    }
+  
+
+    
+
+
+    const crearTienda = await createTienda(data);
  
-/*
-    VER EL TEMA DE LOS UNIQUE PARA PODER AGREGARLO A LA TABLA EN SQL
-
-
-*/
 
 }
 
