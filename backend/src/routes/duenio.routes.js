7b616571d9cf5d9ac1146f213fb128f39f349b2e
Julio-Martel <julioramdom123@gmail.com>
2026-06-26 19:30:47 -0300

    se ha añadido el handler para que el dueño pueda obtener todas sus tiendas

--- backend/src/routes/duenio.routes.js
+++ backend/src/routes/duenio.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { autenticarToken } from '../middlewares/auth.token.js';
 import { permisoDuenio } from '../middlewares/auth.permisos.js';
-import { crearTienda } from '../controllers/duenio.controller.js';
+import { crearTienda, obtenerTiendas } from '../controllers/duenio.controller.js';
 
 const duenioRoutes = express.Router();
 
 duenioRoutes.post('/tienda/crear',autenticarToken, permisoDuenio, crearTienda);
-duenioRoutes.get('/tiendas', autenticarToken, permisoDuenio,/*HANDLER PARA QUE EL DUENIO VEA SUS TIENDAS*/);
+duenioRoutes.get('/tiendas', autenticarToken, permisoDuenio,obtenerTiendas);
 duenioRoutes.get('/tiendas/:id', autenticarToken, permisoDuenio, /*HANDLER FILTRADO DE TIENDAS POR ID*/);
 
 export default duenioRoutes;
