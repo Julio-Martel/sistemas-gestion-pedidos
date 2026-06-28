ff8471180cd076cbe7af0683ad5b4ad82264e53f
Julio-Martel <julioramdom123@gmail.com>
2026-06-28 20:47:43 -0300

    se ha agregado el handler de filtracion de tienda

--- backend/src/routes/duenio.routes.js
+++ backend/src/routes/duenio.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { autenticarToken } from '../middlewares/auth.token.js';
 import { permisoDuenio } from '../middlewares/auth.permisos.js';
-import { crearTienda, obtenerTiendas } from '../controllers/duenio.controller.js';
+import { crearTienda, obtenerTiendas, filtrarTiendas } from '../controllers/duenio.controller.js';
 
 const duenioRoutes = express.Router();
 
 duenioRoutes.post('/tienda/crear',autenticarToken, permisoDuenio, crearTienda);
 duenioRoutes.get('/tiendas', autenticarToken, permisoDuenio, obtenerTiendas);
-//duenioRoutes.get('/tiendas/:id', autenticarToken, permisoDuenio, /*HANDLER FILTRADO DE TIENDAS POR ID*/);
+duenioRoutes.get('/tiendas/:id', autenticarToken, permisoDuenio, filtrarTiendas);
 
 export default duenioRoutes;
