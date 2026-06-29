1875175aea44360c6ebb057a0a91bd999d9d116e
Julio-Martel <julioramdom123@gmail.com>
2026-06-29 09:10:56 -0300

    se ha agregado el handler de creacion de producto

--- backend/src/routes/duenio.routes.js
+++ backend/src/routes/duenio.routes.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { autenticarToken } from '../middlewares/auth.token.js';
 import { permisoDuenio } from '../middlewares/auth.permisos.js';
-import { crearTienda, obtenerTiendas, filtrarTiendas } from '../controllers/duenio.controller.js';
+import { crearTienda, obtenerTiendas, filtrarTiendas, crearProducto } from '../controllers/duenio.controller.js';
 
 const duenioRoutes = express.Router();
 
 duenioRoutes.post('/tienda/crear',autenticarToken, permisoDuenio, crearTienda);
 duenioRoutes.get('/tiendas', autenticarToken, permisoDuenio, obtenerTiendas);
 duenioRoutes.get('/tiendas/:id', autenticarToken, permisoDuenio, filtrarTiendas);
+duenioRoutes.post('/productos/crear', autenticarToken, permisoDuenio, crearProducto);
+
+
+
 
 export default duenioRoutes;
