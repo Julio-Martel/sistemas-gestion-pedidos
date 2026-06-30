d85034abbda203d2c020150b06d28a4646a54132
Julio-Martel <julioramdom123@gmail.com>
2026-06-30 16:26:59 -0300

    se ha importado la funcion de modificar el stock del producto y tambien se agrego el handler del mismo

--- backend/src/routes/duenio.routes.js
+++ backend/src/routes/duenio.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { autenticarToken } from '../middlewares/auth.token.js';
 import { permisoDuenio } from '../middlewares/auth.permisos.js';
-import { crearTienda, obtenerTiendas, filtrarTiendas, crearProducto } from '../controllers/duenio.controller.js';
+import { crearTienda, obtenerTiendas, filtrarTiendas, crearProducto, modificarStockProducto } from '../controllers/duenio.controller.js';
 
 const duenioRoutes = express.Router();
 
@@ -9,7 +9,7 @@ duenioRoutes.post('/tienda/crear',autenticarToken, permisoDuenio, crearTienda);
 duenioRoutes.get('/tiendas', autenticarToken, permisoDuenio, obtenerTiendas);
 duenioRoutes.get('/tiendas/:id', autenticarToken, permisoDuenio, filtrarTiendas);
 duenioRoutes.post('/productos/crear', autenticarToken, permisoDuenio, crearProducto);
-duenioRoutes.post('/productos/modificar', autenticarToken, permisoDuenio, /*HANDLER DE MODIFICAR STOCK DE PRODUCTO*/)
+duenioRoutes.post('/productos/modificar', autenticarToken, permisoDuenio, modificarStockProducto);
 
 
 
