4b5dd3731f5a0b7d9694b108c9fc919bf1dcf393
Julio-Martel <julioramdom123@gmail.com>
2026-07-17 18:04:17 -0300

    se ha agregado una ruta nueva para el filtrado de productos

--- backend/src/routes/clientes.routes.js
+++ backend/src/routes/clientes.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verProductos, crearPedido, verMisPedidos, cancelarPedido } from '../controllers/cliente.controller.js';
+import { verProductos, crearPedido, verMisPedidos, cancelarPedido, filtrarProducto } from '../controllers/cliente.controller.js';
 import { autenticarToken } from '../middlewares/auth.token.js';
 import { permisoCliente } from '../middlewares/auth.permisos.js';
 
@@ -7,6 +7,9 @@ const clientesRoutes = express.Router();
 
 // VER TODOS LOS PRODUCTOS
 clientesRoutes.get('/productos', autenticarToken, permisoCliente, verProductos);
+
+// FILTRAR POR ID DE PRODUCTO
+clientesRoutes.get('productos/:id', autenticarToken, permisoCliente, filtrarProducto);
 
 // CREAR PEDIDO
 clientesRoutes.post('/pedidos/crear', autenticarToken, permisoCliente, crearPedido);
